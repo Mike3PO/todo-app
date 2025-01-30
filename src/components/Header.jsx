308b7eb2ed7f63e54a966403897a8712b761986c
Mike3PO <miken5488@gmail.com>
2025-01-29 22:04:01 -0500

    Updated header to include if number of tasks was plural or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 export function Header(props) {
     const { todos, selectedTab } = props
-    const todosLength = todos.length
-    const isPlural = todosLength != 1 ? 's' : ''
+    const filterTodosList = selectedTab === 'All' ? 
+        todos : 
+        selectedTab === 'Completed' ? 
+            todos.filter(val => val.complete) :
+            todos.filter(val => !val.complete)
+    const numTodosInList = filterTodosList.length
+    const isPlural = numTodosInList != 1 ? 's' : ''
 
     return (
         <header>
-            <h1 className="text-gradient">You have {todosLength} {selectedTab != 'All' && (selectedTab)} task{isPlural}.</h1>
+            <h1 className="text-gradient">You have {numTodosInList} {selectedTab != 'All' && (selectedTab)} task{isPlural}.</h1>
         </header>
     )
 }
